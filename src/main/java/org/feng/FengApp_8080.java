5e363cc3cda783abbe104c21fc223e8cb584b0e0
bayernjf <41980865+bayernjf@users.noreply.github.com>
2024-05-04 12:39:35 +0800

    Update FengApp_8080.java

--- src/main/java/org/feng/FengApp_8080.java
+++ src/main/java/org/feng/FengApp_8080.java
@@ -16,9 +16,6 @@ public class FengApp_8080 {
 	public static void main(String[] args) {
 		SpringApplication.run(FengApp_8080.class, args);
 		// 测试git
-		// 测试git
-		// ceshi git
-//		ceshi git
 	}
 
 }
